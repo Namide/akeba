562f4623b3b4b1390b78e0928af847f52bee307b
Namide <damien@doussaud.fr>
2026-02-23 23:19:35 +0100

    Add menu events

---
+++ src/inputs/MenuEventsManager.ts
@@ -0,0 +1,97 @@
+import { Object3D, PerspectiveCamera, Raycaster, Vector2 } from "three";
+
+type MenuEvent = {
+  meshName: string,
+  eventName: 'click' | 'hover' | 'out',
+  callback: (mesh: Object3D) => void
+}
+
+export class MenuEventsManager {
+
+  camera
+  eventList: MenuEvent[] = []
+  pointer = new Vector2()
+  raycaster = new Raycaster();
+  objects3D: Object3D[] = []
+  hoverObject: Object3D | undefined
+
+  constructor(camera: PerspectiveCamera) {
+    this.camera = camera
+    this.onMove = this.onMove.bind(this)
+    this.onClick = this.onClick.bind(this)
+  }
+
+  addEvent(
+    eventName: MenuEvent['eventName'],
+    meshName: string,
+    callback: MenuEvent['callback']
+  ) {
+    this.eventList.push({ eventName, meshName, callback })
+  }
+
+  disable() {
+    document.removeEventListener('mousemove', this.onMove);
+    document.removeEventListener('click', this.onClick);
+  }
+
+  enable() {
+    this.disable()
+    document.addEventListener('mousemove', this.onMove);
+    document.addEventListener('click', this.onClick);
+  }
+
+  #getHit() {
+    this.raycaster.setFromCamera(this.pointer, this.camera);
+    const intersects = this.raycaster.intersectObjects(this.objects3D, true);
+    return intersects[0]
+  }
+
+  tick() {
+    const lastHover = this.hoverObject
+
+    let newHover: { object3D: Object3D, callback: MenuEvent['callback'] } | undefined
+
+    const hit = this.#getHit()
+    if (hit) {
+      for (const event of this.eventList.filter(event => event.eventName === 'hover')) {
+        if (event.meshName === hit.object.name) {
+          newHover = { object3D: hit.object, callback: event.callback }
+        }
+      }
+    }
+
+    if (newHover && newHover.object3D !== lastHover) {
+      newHover.callback(newHover.object3D)
+    }
+
+    this.hoverObject = newHover?.object3D
+
+    if (lastHover && lastHover !== newHover?.object3D) {
+
+      // (this.hoverObject as Mesh<BufferGeometry, MeshLambertMaterial>).material.color.set('#ffffff')
+
+      this.eventList.find(
+        event => event.eventName === 'out' && event.meshName === lastHover.name
+      )?.callback(lastHover)
+
+      // this.hoverObject = undefined
+    }
+  }
+
+  private onMove(event: MouseEvent) {
+    this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    this.pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  }
+
+  private onClick() {
+    const hit = this.#getHit()
+    console.log('CLICK', hit)
+    if (hit) {
+      for (const event of this.eventList.filter(event => event.eventName === 'click')) {
+        if (event.meshName === hit.object.name) {
+          event.callback(hit.object)
+        }
+      }
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,9 @@ import { createCharacter } from "./entities/createCharacter";
 import { createCameraPosition } from "./render/cameraPosition";
 import { createCharacterControls } from "./gameplay/createCharacterControls";
 import { createTrack } from "./entities/createTrack";
-import { Object3D, PerspectiveCamera, Vector3 } from 'three';
+import { BufferGeometry, Mesh, MeshLambertMaterial, Object3D, PerspectiveCamera, Vector3 } from 'three';
 import { createPhysicListener } from './physic/createPhysicListener';
+import { MenuEventsManager } from './inputs/MenuEventsManager';
 
 let isPlaying = true
 
@@ -50,7 +51,7 @@ const physicListener = createPhysicListener([
   }
 ])
 
-
+const menuEventManager = new MenuEventsManager(render.camera)
 
 const startTime = Date.now()
 attachTick(({ deltaS }) => {
@@ -61,6 +62,8 @@ attachTick(({ deltaS }) => {
     if (cameraPosition.gameTick) {
       cameraPosition.gameTick()
     }
+  } else {
+    menuEventManager.tick()
   }
 
   if (cameraPosition.tick) {
@@ -77,26 +80,58 @@ attachTick(({ deltaS }) => {
 
 // Screens
 
-const changeScreen = (screen: 'controls' | 'home' | 'credits' | 'play') => {
+
+const gotToHome = () => changeScreen('home')
+const gotToControls = () => changeScreen('controls')
+const gotToCredits = () => changeScreen('credits')
+const gotPlay = () => changeScreen('play')
+
+const setHover = (obj: Object3D) => (obj as Mesh<BufferGeometry, MeshLambertMaterial>).material.color.set('#ffff00')
+const setOut = (obj: Object3D) => (obj as Mesh<BufferGeometry, MeshLambertMaterial>).material.color.set('#ffffff')
+
+menuEventManager.addEvent('hover', 'button-play', setHover)
+menuEventManager.addEvent('out', 'button-play', setOut)
+menuEventManager.addEvent('click', 'button-play', gotPlay)
+menuEventManager.addEvent('hover', 'button-controls', setHover)
+menuEventManager.addEvent('out', 'button-controls', setOut)
+menuEventManager.addEvent('click', 'button-controls', gotToControls)
+menuEventManager.addEvent('hover', 'button-credits', setHover)
+menuEventManager.addEvent('out', 'button-credits', setOut)
+menuEventManager.addEvent('click', 'button-credits', gotToCredits)
+menuEventManager.addEvent('hover', 'button-return', setHover)
+menuEventManager.addEvent('out', 'button-return', setOut)
+menuEventManager.addEvent('click', 'button-return', gotToHome)
+menuEventManager.addEvent('hover', 'button-return-2', setHover)
+menuEventManager.addEvent('out', 'button-return-2', setOut)
+menuEventManager.addEvent('click', 'button-return-2', gotToHome)
+
+function changeScreen(screen: 'controls' | 'home' | 'credits' | 'play') {
   const objectsAdd: Object3D[] = []
   const objectsRemove: Object3D[] = []
 
   switch (screen) {
     case 'play':
+      menuEventManager.disable()
       objectsRemove.push(controlMeshes, homeMeshes, creditsMeshes)
       isPlaying = true
       break
     case 'home':
+      menuEventManager.enable()
+      menuEventManager.objects3D = [homeMeshes]
       objectsAdd.push(homeMeshes)
       objectsRemove.push(controlMeshes, creditsMeshes)
       isPlaying = false
       break
     case 'credits':
+      menuEventManager.enable()
+      menuEventManager.objects3D = [creditsMeshes]
       objectsAdd.push(creditsMeshes)
       objectsRemove.push(controlMeshes, homeMeshes)
       isPlaying = false
       break
     case 'controls':
+      menuEventManager.enable()
+      menuEventManager.objects3D = [controlMeshes]
       objectsAdd.push(controlMeshes)
       objectsRemove.push(homeMeshes, creditsMeshes)
       isPlaying = false
@@ -119,13 +154,13 @@ const changeScreen = (screen: 'controls' | 'home' | 'credits' | 'play') => {
     render.camera.updateProjectionMatrix();
 
     for (const object3D of objectsAdd) {
-      const pos = camera.getWorldPosition(new Vector3())
+      const pos = render.camera.getWorldPosition(new Vector3())
       pos.add(
-        camera.getWorldDirection(new Vector3()).multiplyScalar(3)
+        render.camera.getWorldDirection(new Vector3()).multiplyScalar(3)
       )
 
       object3D.position.copy(pos)
-      object3D.quaternion.copy(camera.quaternion)
+      object3D.quaternion.copy(render.camera.quaternion)
     }
   }
 
@@ -133,7 +168,7 @@ const changeScreen = (screen: 'controls' | 'home' | 'credits' | 'play') => {
   render.scene.add(...objectsAdd)
 }
 
-changeScreen('play')
+changeScreen('home')
 
 
 // return {
