8463d1f298166c69e9909d712410e72d1e220c92
Namide <damien@doussaud.fr>
2026-02-24 23:34:37 +0100

    Enable debug on prod

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const RESOLUTION_HEIGHT = 240
-export const DEBUG = false
+export const DEBUG = false || (window.location.search === '?debug')
 export const LIGHT_SCALE_MIN = 0.25
 export const LIGHT_SCALE_MAX = 1
